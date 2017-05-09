9d96c4e6af61d72619d7778eb53eb4568e7996df
James <james.a.bromley@googlemail.com>
2017-05-09 04:26:33 +0100

    updated samples

--- script/score.js
+++ script/score.js
@@ -13,7 +13,7 @@ function ScoreLoader(settings) {
                 times: ["0:1/4", "0:2/4", "0:3/4", "1:1/4", "1:2/4", "1:3/4", "2:1/4", "2:2/4", "2:3/4", "3:1/4", "3:2/4", "3:3/4"]
             },
             "snare": {src: "audio/snare2.wav", times: ["1:1/4", "3:1/4"]},
-            "bork": {src: "audio/pling.wav", times: []},
+            "bork": {src: "audio/siren.wav", times: []},
             "beep": {src: "audio/bass.wav", times: []}
         }
     }, {
